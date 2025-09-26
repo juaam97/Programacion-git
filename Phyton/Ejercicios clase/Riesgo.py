64619e67a0d008667e8fc61d1bb08db7a642fdbf
juaam97 <jmagmej503@g.educaand.es>
2025-09-26 12:29:12 +0200

    Actualizacion

---
+++ Phyton/Ejercicios clase/Riesgo.py
@@ -0,0 +1,11 @@
+Historialcrediticio = float(input("Introduce un numero negativo si tu historial es negativo o uno positivo si tu historial es positivo"))
+Empleo = float(input("Introduce cuantos años estas trabajando:"))
+Solicitud = float(input("Introduce la cantidad de tu solicitud:"))
+Sueldo = float(input("Introduce tu sueldo:"))
+
+
+if Historialcrediticio < 0 or Empleo < 2 or Solicitud <= 1% Sueldo :
+    print ("Es de riesgo")
+else : 
+    print ("No es de riesgo")
+print ("gracias por venir")    
